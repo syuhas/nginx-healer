7275d1a7c8ef589020071718d8f44fd415a74722
syuhas <syuhas22@gmail.com>
2025-04-23 17:20:19 -0400

    docker-compose fix

--- deploy/build.sh
+++ deploy/build.sh
@@ -8,7 +8,7 @@ sudo usermod -aG docker ec2-user
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo chmod 777 /var/run/docker.sock
-sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/dock>sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
 
